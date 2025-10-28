32f74cb8193a6bc4a0e2951314b90f2d6609136b
Phan Công Phước <phancongphuoc241204@gmail.com>
2025-10-29 00:37:18 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _125LTWNC_BCK.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
